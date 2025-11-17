1e18ad70ef9eefac96dac39c8d4cf60cdf5050d5
Fer1723 <fercoarteaga314@gmail.com>
2025-11-17 10:04:23 -0600

    Fix: Update API_DATA

--- data.py
+++ data.py
@@ -3,32 +3,86 @@ CANCIONES_DATA = [
         "id": 1,
         "titulo": "There's Only You",
         "artista": "Above & Beyond",
-        "pensamiento": "Me hace pensar en ti en mis momentos mas dificiles",
+        "pensamiento": "Me hace pensar en ti en mis momentos mas dificiles.",
         "youtube_url": "https://www.youtube.com/watch?v=AQ3tw7I42dA&list=RDAQ3tw7I42dA&start_radio=1", 
         "spotify_id": "638ZYZKHJFGgAircZnBRPl" 
     },
     {
         "id": 2,
-        "titulo": "Blinding Lights",
-        "artista": "The Weeknd",
-        "genero": "Pop",
-        "youtube_url": "https://www.youtube.com/watch?v=4YlC8rVlKj8",
-        "spotify_id": "0Vjs0yXFqL1N6d7N2nQ19i"
+        "titulo": "Here With Me",
+        "artista": "D4vd",
+        "pensamiento": "Me hace saber lo unico que quiero contigo.",
+        "youtube_url": "https://www.youtube.com/watch?v=Ip6cw8gfHHI&list=RDIp6cw8gfHHI&start_radio=1",
+        "spotify_id": "0NLm9bQG7ikL5k9x9TtYT7"
     },
     {
         "id": 3,
-        "titulo": "Hotel California (Live)",
-        "artista": "Eagles",
-        "genero": "Rock",
-        "youtube_url": "https://www.youtube.com/watch?v=wX-y5E3TqN4",
-        "spotify_id": "3v37o37g3y86o4c8m570b5"
+        "titulo": "Better Hafl of Me",
+        "artista": "Dash Berlin",
+        "pensamiento": "Me recuerda lo hermoso que es estar contigo y la falta que me hacias.",
+        "youtube_url": "https://www.youtube.com/watch?v=PEqRxZjC2rM&list=RDPEqRxZjC2rM&start_radio=1",
+        "spotify_id": "3ZaW10BRZMxOTOXiN8PtTn"
     },
     {
         "id": 4,
-        "titulo": "Smooth Criminal",
-        "artista": "Michael Jackson",
-        "genero": "Pop",
-        "youtube_url": "https://www.youtube.com/watch?v=h_D3VFfhvs4",
-        "spotify_id": "6V18jNf470V5uR0v48j4Hn"
+        "titulo": "Need to feel love",
+        "artista": "Adam K & Soha",
+        "pensamiento": "Me hace pensar en lo unico que quiero de ti.",
+        "youtube_url": "https://www.youtube.com/watch?v=h6lbYvoG99g&list=RDh6lbYvoG99g&start_radio=1",
+        "spotify_id": "4b9S5yZpvIVxMEuI0rHzk"
+    },
+    {
+        "id": 5,
+        "titulo": "Gratitude",
+        "artista": "Adbove & Betond",
+        "pensamiento": "Me recuerda lo hermoso que hiciste el mundo desde que llegaste a mi vida",
+        "youtube_url": "https://www.youtube.com/watch?v=CF2ebEXi-cw&list=RDCF2ebEXi-cw&start_radio=1",
+        "spotify_id": "0KU8W0lHfsNlH7lfV1dz29?"
+
+    },
+    {
+        "id": 6,
+        "titulo": "La Magia",
+        "artista": "Little Jesus",
+        "pensamiento": "ME ace pensar como tu eres mi refugio de todo en este mundo.",
+        "youtube_url": "https://www.youtube.com/watch?v=6Gfdm2E1XZY&list=RD6Gfdm2E1XZY&start_radio=1",
+        "spotify_id": "3XJmD5suzFUBHJ7sizBAiy"
+
+    },
+    {
+        "id": 7,
+        "titulo": "L'amour Toujours",
+        "artista": "Dzeko & Torres & Torres",
+        "pensamiento": "Me hace sentir que nuestro amor, es unico, es lo que me hace moverme.",
+        "youtube_url": "https://www.youtube.com/watch?v=g2rdrNONy7k&list=RDg2rdrNONy7k&start_radio=1",
+        "spotify_id": "4UutBLYMby6whu2bMYh9Oa"
+
+    },
+    {
+        "id": 8,
+        "titulo": "Leave my hand",
+        "artista": "MaRLo",
+        "pensamiento": "Me recuerda a todo lo que pasamos y como tu siempre sera mi promesa de que jamas ire a donde sea solo",
+        "youtube_url": "https://www.youtube.com/watch?v=AyjPw4CKj5g&list=RDAyjPw4CKj5g&start_radio=1",
+        "spotify_id": "6yvHeT9lVpJ58y8WDL8F7r"
+
+    },
+    {
+        "id": 9,
+        "titulo": "You and Me",
+        "artista": "MaRLo",
+        "pensamiento": "Me recuerda como tu eres, en este mundo caotico, lo unico que complementa mi universo, como somos Tu & Yo",
+        "youtube_url": "https://www.youtube.com/watch?v=11zl0Cvb4Ng&list=RD11zl0Cvb4Ng&start_radio=1",
+        "spotify_id": "2AAMgG3DPe0VSaGOBRjuKj"
+
+    },
+    {
+        "id": 10,
+        "titulo": "For all Time",
+        "artista": "Armin Van Buuren",
+        "pensamiento": "Me recuerda como es que llegaste, como el destino te puso para que te quedes por todo el tiempo",
+        "youtube_url": "https://www.youtube.com/watch?v=ZvffLHvbunc&list=RDZvffLHvbunc&start_radio=1",
+        "spotify_id": "2mFXiHWO51YlBQyNYGcMsd"
+
     }
 ]
